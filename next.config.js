8a679d2eb3962bc51b2ad634d12b11f44c2f8877
ckuzmick <conrad.kuzmick@icloud.com>
2023-11-24 22:37:16 -0500

    main

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'storage.googleapis.com',
+                port: '',
+                pathname: '/nflranked-theringer-com-cms/public/media/ringernflranked/players/BrockPurdy-updated-1695670083966-large.png'
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
